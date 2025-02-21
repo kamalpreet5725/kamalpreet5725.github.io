071ab71492386b3ec942c9c1b27106d5a5a9bbbd
kamalpreet5725 <kamalpreetgill8360@gmail.com>
2025-02-21 10:31:10 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+height = float(input("Enter the height of a triangle:"))
+base  = float(input("Enter the base of a triangle:"))
+
+area  = (0.5)*height*base 
+
+print("The area of triangle is", area)
---
+++ even odd.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number "))
+
+if a % 2==0:
+ print("number is even ")
+
+else:
+ print("Number is odd ") 
